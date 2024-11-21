12b7aa89cf2c573a4077f3f14c5654a01800e266
Vishal patel <142934301+Vishalxp20@users.noreply.github.com>
2024-11-21 18:59:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ const App = () => {
   const [responseData, setResponseData] = useState(null);
 
   return (
-    <div style={{display:"flex",flexDirection:"column", justifyContent:"center",alignItems:"center",gap:"5px"}}>
-      <h1 style={{background:"grey",borderRadius:"10px", padding:"5px 5px"}}>0827AL211063</h1>
+    <div >
+      <h1 >0832CS211199</h1>
       <InputForm setResponseData={setResponseData} />
       {responseData && <ResponseDisplay data={responseData} />}
     </div>
